b5881dac43400a347fb3ca119937cc8d84e19d6c
hitalo <hitalo.2014.hs@gmail.com>
2022-12-21 11:12:13 -0300

    fix: cors headers

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import "express-async-errors";
 import "reflect-metadata";
 import { handleAsyncErrorResponseMiddleware } from "./middlewares/handleAsyncErrorResponseMiddleware";
 import { appRoutes } from "./routes";
-
-export const app = express();
-app.use(express.json());
-
-appRoutes(app)
-app.use(handleAsyncErrorResponseMiddleware)
-
 const options: cors.CorsOptions = {
   methods: "GET, POST, PATCH, DELETE",
   origin: "*",
 };
+
+export const app = express();
+app.use(express.json());
 app.use(cors(options));
+
+appRoutes(app)
+app.use(handleAsyncErrorResponseMiddleware)
+
